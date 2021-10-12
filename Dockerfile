6acfbbc3d9388b8949682b3c18c62ac54f2ca7f0
seano-vs <sean.mccully@voteshield.us>
2021-10-12 12:53:03 -0400

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,62 @@
+# Container image that runs your code
+# FROM alpine:latest
+
+FROM public.ecr.aws/bitnami/python:3.9-prod
+
+# build dependencies
+# 
+ENV BUILD_PACKAGES="g++ libpq-dev curl git"
+
+# Install build deps and heavy dependencies
+#
+RUN apt-get update
+RUN apt-get install --no-install-recommends -y ${BUILD_PACKAGES}
+
+# Environment values
+#
+ENV PYTHONUNBUFFERED=1 \
+    # pip
+    PIP_NO_CACHE_DIR=off \
+    PIP_DISABLE_PIP_VERSION_CHECK=on \
+    PIP_DEFAULT_TIMEOUT=100 \
+    \
+    # poetry
+    # https://python-poetry.org/docs/configuration/#using-environment-variables
+    # make poetry install to this location
+    POETRY_HOME="/opt/poetry" \
+    # make poetry create the virtual environment in the project's root
+    # it gets named `.venv`
+    POETRY_VIRTUALENVS_IN_PROJECT=true \
+    # do not ask any interactive question
+    POETRY_NO_INTERACTION=1 \
+    \
+    # paths
+    # this is where our requirements + virtual environment will live
+    PYSETUP_PATH="/opt/pysetup" \
+    VENV_PATH="/opt/pysetup/.venv"
+
+
+# prepend poetry and venv to path
+ENV PATH="/opt/poetry/bin:/opt/pysetup/.venv/bin:$PATH"
+
+
+# install poetry - respects $POETRY_VERSION & $POETRY_HOME
+RUN curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
+
+# copy project requirement files here to ensure they will be cached.
+WORKDIR $PYSETUP_PATH
+COPY poetry.lock pyproject.toml ./
+
+# install runtime deps - uses $POETRY_VIRTUALENVS_IN_PROJECT internally
+RUN poetry install --no-dev
+
+
+# Everything before this came from the Inspector 
+
+# Copies your code file from your action repository to the filesystem path `/` of the container
+COPY src /
+
+WORKDIR "/src"
+
+ENTRYPOINT [ "/opt/pysetup/.venv/bin/python" ]
+CMD ["/opt/pysetup/.venv/bin/pytest", "-v", "vape.py"]
---
+++ src/vape.py
@@ -0,0 +1,62 @@
+from hypothesis import settings, Phase
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+import urllib.request
+import schemathesis
+import os
+
+DEFAULT_TIMEOUT = 10
+environment = os.environ.get('ENVIRONMENT', 'goatfield')
+base_url = os.environ.get('BASE_URL', 'https://api-vs.goatfield.us')
+
+if os.path.exists('apikeys.json'):
+    with open('apikeys.json') as json_file:
+        keysdict = json.load(json_file)
+else:
+    keysdict = os.environ.get('VS_API_KEY')
+
+# This is required because of this:
+# https://github.com/flasgger/flasgger/issues/267
+jsonurl = urllib.request.urlopen("{}/v1/spec.json".format(base_url))
+schema_dict = json.loads(jsonurl.read())
+schema_dict.pop('definitions', None)
+schema = schemathesis.from_dict(
+    schema_dict, 
+    base_url=base_url, 
+    skip_deprecated_operations=True,
+)
+
+# Get our access token by exchanging our keys for it.
+def bearer_access_token():
+    query = {
+        "grant_type": "refresh_token",
+        "client_id": keysdict['ClientId'],
+        "refresh_token": keysdict['refresh_token'],
+        "scope": "CognitoGroup/Developers",
+    }
+    r = requests.post(
+        "https://oauth.{}.us/oauth2/token".format(environment),
+        data=query,
+        auth=HTTPBasicAuth(keysdict['ClientId'], keysdict['ClientSecret']),
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    )
+    response = r.json()
+    return response['access_token']
+
+
+# For now, population and tag routes are excluded.
+#@schema.parametrize(endpoint="^/(?!populations|tags)",)
+@schema.parametrize(method="GET")
+@settings(phases=[Phase.explicit], deadline=None)
+def test_api(case):
+    # Slow routes are defined here. Don't really know of a way to not hard code these.
+    timeout = {
+        ("GET", "/changes"): 60,
+        ("GET", "/change_history"): 60,
+        ("GET", "/download_population"): 60,
+        ("GET", "/system/snapshots"): 60
+    }.get((case.operation.method.upper(), case.operation.path), DEFAULT_TIMEOUT)
+    case.headers = case.headers or {}
+    case.headers["Authorization"] = "Bearer " + f"{bearer_access_token()}"
+    case.call_and_validate(timeout=timeout)
